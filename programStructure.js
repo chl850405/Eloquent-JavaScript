90a0dfbee10542ec4e8387e8e669d2ea267b8389
chl850405 <christimarchetti@gmail.com>
2022-03-29 18:59:39 -0400

    final refactor fizz buzz

--- programStructure.js
+++ programStructure.js
@@ -223,7 +223,7 @@ fizzBuzz = () => {
       console.log("fizz-buzz")
       }
     //if num in none of the above print number
-    else if((i != 3 == 0) && (i != 5 == 0)){
+    else{
     console.log(i)
     }
   }
